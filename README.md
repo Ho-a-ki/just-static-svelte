35619f563e4b17ef1efaef30f3b9c0377a560d27
gh <gh4777@naver.com>
2022-02-27 17:37:58 +0900

    내비게이션 바

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
+# just.swiss 클론코딩 하기.
 
+1. nav바 만들기
+   navbar 만들기 전에 grid 시스템으로 navbar도 할 수 있는지?
+   모바일 먼저 만들고 => PC 만들기.
+   nav바는 그냥 flex를 쓰고, 안에 레이아웃만 grid로.
 
-## 만들어보자
+2. 그리드
+   https://nykim.work/59
 
-# 수정해보자
+- div의 크기를 지정한 후에 img 속성 max-width에 100%를 걸어줘야 상위 노드의 크기에 한정적으로 작동함.
+
+3. 프리티어 설치하기 및 세팅
+   https://velog.io/@byjihye/svelte-install
---
+++ site/src/App.svelte
@@ -0,0 +1,157 @@
+<script>
+  import { fade } from "svelte/transition";
+  let mobileMenuView = true;
+
+  function toggleMobileMenu() {
+    mobileMenuView = !mobileMenuView;
+  }
+</script>
+
+<style>
+  @import url("https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css");
+  @import url("https://fonts.googleapis.com/css2?family=Noto+Serif+KR:wght@300&display=swap");
+  main {
+    --main-color: #2553a0;
+    font-family: Pretendard;
+  }
+
+  header {
+    height: 114px;
+    background: rgb(241, 241, 241);
+    padding: 12px 0 12px;
+  }
+
+  #logo {
+    position: absolute;
+    width: 134px;
+    left: 20px;
+    top: 20px;
+  }
+
+  .menu-right {
+    position: absolute;
+    right: 30px;
+    top: 30px;
+    text-align: center;
+    color: var(--main-color);
+  }
+
+  img {
+    max-width: 100%;
+  }
+
+  #menu {
+    position: absolute;
+    right: 30px;
+    top: 30px;
+    text-align: center;
+    color: var(--main-color);
+  }
+
+  i {
+    font-size: 32px;
+  }
+
+  .mobile-menu {
+    position: fixed;
+    width: 100vw;
+    height: 100vh;
+    background: var(--main-color);
+    padding: 0;
+  }
+
+  ul.mobile-menu-list {
+    padding: 20px;
+  }
+
+  nav ul {
+    /* display: none; */
+    list-style: none;
+    padding: 0;
+  }
+  .fa-times {
+    color: white;
+  }
+
+  nav a:link,
+  nav a:visited {
+    font-family: "Noto Serif KR", serif;
+    text-decoration: none;
+    display: block;
+    padding: 10px;
+    color: rgb(255, 255, 255);
+  }
+
+  nav a:hover {
+    background-color: rgba(207, 232, 220, 0.7);
+  }
+
+  .close {
+    text-align: right;
+    display: block;
+    padding: 20px 0;
+  }
+
+  @media screen and (min-width: 40em) {
+    nav ul {
+      display: flex;
+    }
+
+    nav li {
+      flex: 1;
+    }
+  }
+
+  @media screen and (min-width: 70em) {
+    main {
+      display: grid;
+      grid-template-columns: 3fr 1fr;
+      column-gap: 20px;
+    }
+  }
+</style>
+
+<main>
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"
+  />
+  <div class="wrapper">
+    <header>
+      <div id="logo">
+        <a href="/#">
+          <img src="images/top-logo.svg" alt="" />
+        </a>
+      </div>
+      <div id="menu" class="menu-right" on:click="{toggleMobileMenu}">
+        <div>
+          <i class="fa fa-bars" aria-hidden="true"></i>
+        </div>
+        <span>MENU</span>
+      </div>
+      {#if mobileMenuView}
+        <div transition:fade="{{ duration: 150 }}" class="mobile-menu">
+          <nav>
+            <ul class="mobile-menu-list">
+              <div class="close" on:click="{toggleMobileMenu}">
+                <i class="fa fa-times" aria-hidden="true"></i>
+              </div>
+              <li><a href="/#">About</a></li>
+              <li><a href="/#">Contact</a></li>
+              <li><a href="/#">Meet the team</a></li>
+              <li><a href="/#">Blog</a></li>
+            </ul>
+          </nav>
+        </div>
+      {/if}
+      <!-- <nav>
+        <ul>
+			  <li><a href="">About</a></li>
+			  <li><a href="">Contact</a></li>
+			  <li><a href="">Meet the team</a></li>
+			  <li><a href="">Blog</a></li>
+			</ul>
+		  </nav> -->
+    </header>
+  </div>
+</main>
